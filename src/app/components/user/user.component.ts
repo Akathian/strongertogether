606979844f03d3430ced82fcf00a378411b82fee
Akathian <akathian05@gmail.com>
2020-10-31 23:29:03 -0400

    fix

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user;
-  userLoggedIn = false;
+  userLoggedIn = true;
   section;
   sections = [
     { name: 'Profile', id: 'profile' },
